4a9abe0111667bcfd317967679eadb088a7689e1
disruptive pattern material <nathan@tableman.com>
2025-09-23 21:19:00 -0700

    Update hub75_display.h

--- components/hub75_base/hub75_display.h
+++ components/hub75_base/hub75_display.h
@@ -15,7 +15,7 @@ using esphome::esp_log_printf_;
 // This is needed for the underlaying library "ESP32 HUB75 LED MATRIX PANEL DMA Display"
 
 
-#include <MatrixPanel_I2S_DMA.h>
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
 namespace esphome {
 namespace hub75_base {
